89af1ef7a16dbcd6a6972ae8cb7716296bdca095
Doug Parker <dgp1130@users.noreply.github.com>
2020-05-28 22:25:54 -0700

    Updates Greeter extension to use generated BXPB descriptors.
    Refs #1.
    
    This uses the new descriptors generated by the `bxpb-protoc-plugin` rather than the hand-written descriptors.

--- examples/greeter/src/background.ts
+++ examples/greeter/src/background.ts
@@ -1,9 +1,9 @@
 import { GreetRequest, GreetResponse } from '../proto/foo/bar/greeter_pb';
 import { serve } from 'bxpb-runtime/dist/service';
-import { greeterService } from './greeter_service';
+import { GreeterService } from '../proto/foo/bar/greeter_bxdescriptors';
 
 // Run and implement GreeterService.
-serve(chrome.runtime.onMessage, greeterService, {
+serve(chrome.runtime.onMessage, GreeterService, {
     async Greet(req: GreetRequest): Promise<GreetResponse> {
         const res = new GreetResponse();
         res.setMessage(`Hello, ${req.getName()}!`);
--- examples/greeter/src/greeter_client.ts
+++ examples/greeter/src/greeter_client.ts
@@ -2,11 +2,11 @@
 
 import { ProtoClient, rpc } from 'bxpb-runtime/dist/client';
 import { GreetRequest, GreetResponse } from '../proto/foo/bar/greeter_pb';
-import { greeterService } from './greeter_service';
+import { GreeterService } from '../proto/foo/bar/greeter_bxdescriptors';
 
 /** Client for calling the `Greeter` service. */
 export class GreeterClient extends ProtoClient {
     async Greet(req: GreetRequest): Promise<GreetResponse> {
-        return await rpc(this.sendMessage, greeterService, greeterService.methods.Greet, req) as GreetResponse;
+        return await rpc(this.sendMessage, GreeterService, GreeterService.methods.Greet, req) as GreetResponse;
     }
 }
