dc546c490b8d8405363ba7b5aa2b50b4141e9876
Doug Parker <dgp1130@users.noreply.github.com>
2020-05-29 22:36:46 -0700

    Adds service generation.
    Refs #1.
    
    This generates the service code for a proto source file. For the most part, this is just a call through to `bxpb-runtime`'s generic `serve()` function. However, this allows users to only worry about the generated code and not need additional mental overhead to import and use the runtime as well. The format here mostly follows the way the descriptors are generated, but a little simpler because there is just less to generate for each service.

---
+++ packages/bxpb-protoc-plugin/src/generators/services.ts
@@ -0,0 +1,83 @@
+import * as path from 'path';
+import { FileDescriptorProto } from "google-protobuf/google/protobuf/descriptor_pb";
+import { CodeGeneratorResponse } from "google-protobuf/google/protobuf/compiler/plugin_pb";
+
+/**
+ * Returns an iterable of generated BXPB services from the given *.proto file and its descriptor.
+ */
+export function* generateServiceFiles(file: string, fileDescriptor: FileDescriptorProto):
+        Iterable<CodeGeneratorResponse.File> {
+    // If there are no services, don't generate anything.
+    if (fileDescriptor.getServiceList().length === 0) return;
+    
+    const filePath = path.parse(file);
+    const generatedBaseName = `${filePath.name}_bxservices`;
+
+    // Generate JavaScript file.
+    const jsFile = new CodeGeneratorResponse.File();
+    jsFile.setName(path.format({
+        ...filePath,
+        base: undefined, // Must not specify base for name and ext to be used.
+        name: generatedBaseName,
+        ext: '.js',
+    }));
+    jsFile.setContent(generateServiceJs(filePath, fileDescriptor));
+    yield jsFile;
+
+    // Generate TypeScript definitions.
+    const dtsFile = new CodeGeneratorResponse.File();
+    dtsFile.setName(path.format({
+        ...filePath,
+        base: undefined, // Must not specify base for name and ext to be used.
+        name: generatedBaseName,
+        ext: '.d.ts',
+    }));
+    dtsFile.setContent(generateServiceDts(filePath, fileDescriptor));
+    yield dtsFile;
+}
+
+/** Returns the generated JavaScript source as a string. */
+function generateServiceJs(filePath: path.ParsedPath, descriptor: FileDescriptorProto): string {
+    const serviceNames = descriptor.getServiceList().map((service) => {
+        const name = service.getName();
+        if (!name) throw new Error(`${path.format(filePath)}: Service has no name.`);
+        return name;
+    });
+
+    return `
+import { serve } from 'bxpb-runtime/dist/service.js';
+import * as descriptors from './${filePath.name}_bxdescriptors.js';
+
+${serviceNames.map((serviceName) => `
+/**
+ * Run {@link ${serviceName}Service} on the given transport endpoint, using the provided
+ * implementation for each RPC method.
+ */
+export function serve${serviceName}(transport, serviceImpl) {
+    serve(transport, descriptors.${serviceName}Service, serviceImpl);
+}
+`.trim()).join('\n\n')}
+    `.trim();
+}
+
+/** Returns the generated TypeScript definitions as a string. */
+function generateServiceDts(filePath: path.ParsedPath, descriptor: FileDescriptorProto): string {
+    const serviceNames = descriptor.getServiceList().map((service) => {
+        const name = service.getName();
+        if (!name) throw new Error(`${path.format(filePath)}: Service has no name.`);
+        return name;
+    });
+
+    return `
+import { Transport, ServiceImplementation } from 'bxpb-runtime/dist/service';
+import * as descriptors from './${filePath.name}_bxdescriptors';
+
+${serviceNames.map((serviceName) => `
+/**
+ * Run {@link ${serviceName}Service} on the given transport endpoint, using the provided
+ * implementation for each RPC method.
+ */
+export function serve${serviceName}(transport: Transport, serviceImpl: ServiceImplementation<descriptors.${serviceName}Service>);
+`.trim()).join('\n\n')}
+    `.trim();
+}
---
+++ packages/bxpb-protoc-plugin/src/generators/services_spec.ts
@@ -0,0 +1,128 @@
+import { dummyFileDescriptor } from "../testing/dummies";
+import { generateServiceFiles } from "./services";
+
+describe('services', () => {
+    describe('generateServiceFiles()', () => {
+        it('generates proto service files', () => {
+            const fileDescriptor = dummyFileDescriptor({
+                services: [
+                    {
+                        name: 'Foo',
+                    },
+                ],
+            });
+
+            const [ jsFile, dtsFile ] = Array.from(
+                generateServiceFiles('foo.proto', fileDescriptor));
+            
+            expect(jsFile.getName()).toBe('foo_bxservices.js');
+            expect(jsFile.getContent()).toBe(`
+import { serve } from 'bxpb-runtime/dist/service.js';
+import * as descriptors from './foo_bxdescriptors.js';
+
+/**
+ * Run {@link FooService} on the given transport endpoint, using the provided
+ * implementation for each RPC method.
+ */
+export function serveFoo(transport, serviceImpl) {
+    serve(transport, descriptors.FooService, serviceImpl);
+}
+            `.trim());
+
+            expect(dtsFile.getName()).toBe('foo_bxservices.d.ts');
+            expect(dtsFile.getContent()).toBe(`
+import { Transport, ServiceImplementation } from 'bxpb-runtime/dist/service';
+import * as descriptors from './foo_bxdescriptors';
+
+/**
+ * Run {@link FooService} on the given transport endpoint, using the provided
+ * implementation for each RPC method.
+ */
+export function serveFoo(transport: Transport, serviceImpl: ServiceImplementation<descriptors.FooService>);
+            `.trim());
+        });
+        
+        it('generates multiple services in a single file', () => {
+            const fileDescriptor = dummyFileDescriptor({
+                services: [
+                    { name: 'Foo' },
+                    { name: 'Bar' },
+                ],
+            });
+
+            const [ jsFile, dtsFile ] = Array.from(generateServiceFiles(
+                'foo.proto', fileDescriptor));
+            
+            expect(jsFile.getContent()).toBe(`
+import { serve } from 'bxpb-runtime/dist/service.js';
+import * as descriptors from './foo_bxdescriptors.js';
+
+/**
+ * Run {@link FooService} on the given transport endpoint, using the provided
+ * implementation for each RPC method.
+ */
+export function serveFoo(transport, serviceImpl) {
+    serve(transport, descriptors.FooService, serviceImpl);
+}
+
+/**
+ * Run {@link BarService} on the given transport endpoint, using the provided
+ * implementation for each RPC method.
+ */
+export function serveBar(transport, serviceImpl) {
+    serve(transport, descriptors.BarService, serviceImpl);
+}
+            `.trim());
+
+            expect(dtsFile.getContent()).toBe(`
+import { Transport, ServiceImplementation } from 'bxpb-runtime/dist/service';
+import * as descriptors from './foo_bxdescriptors';
+
+/**
+ * Run {@link FooService} on the given transport endpoint, using the provided
+ * implementation for each RPC method.
+ */
+export function serveFoo(transport: Transport, serviceImpl: ServiceImplementation<descriptors.FooService>);
+
+/**
+ * Run {@link BarService} on the given transport endpoint, using the provided
+ * implementation for each RPC method.
+ */
+export function serveBar(transport: Transport, serviceImpl: ServiceImplementation<descriptors.BarService>);
+            `.trim());
+        });
+
+        it('generates nothing if there are no services in the file', () => {
+            const fileDescriptor = dummyFileDescriptor({
+                services: [], // No services in the file.
+            });
+
+            const generatedFiles = Array.from(generateServiceFiles('foo.proto', fileDescriptor));
+
+            expect(generatedFiles).toEqual([]);
+        });
+
+        it('generates files in subdirectory', () => {
+            const [ jsFile, dtsFile ] = Array.from(generateServiceFiles(
+                'hello/world/foo.proto', dummyFileDescriptor()));
+            
+            // Output files should be in the same subdirectory.
+            expect(jsFile.getName()).toBe('hello/world/foo_bxservices.js');
+            expect(dtsFile.getName()).toBe('hello/world/foo_bxservices.d.ts');
+        });
+
+        it('throws when given a service with no name', () => {
+            const fileDescriptor = dummyFileDescriptor({
+                services: [
+                    { name: null }, // Caller forgot to set a name.
+                ],
+            });
+
+            expect(() => Array.from(generateServiceFiles('foo.proto', fileDescriptor)))
+                .toThrowMatching(
+                    (err: Error) => err.message.includes('foo.proto: Service has no name.'),
+                )
+            ;
+        });
+    });
+});
