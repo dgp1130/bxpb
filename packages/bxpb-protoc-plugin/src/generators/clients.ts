7c483ef7011b427827c73d2a455f4aa73d8ebd29
Doug Parker <dgp1130@users.noreply.github.com>
2020-05-31 21:27:16 -0700

    Adds clients generator.
    Refs #1.
    
    This generates the JavaScript and TypeScript definition code for BXPB clients. Relatively straightforward implementation after going through services and descriptors. There is a bit more code to generate which adds some complexity, in particular because this deals with both services **and** methods, unlike generated service code.

---
+++ packages/bxpb-protoc-plugin/src/generators/clients.ts
@@ -0,0 +1,93 @@
+import * as path from 'path';
+import { FileDescriptorProto } from 'google-protobuf/google/protobuf/descriptor_pb';
+import { CodeGeneratorResponse } from 'google-protobuf/google/protobuf/compiler/plugin_pb';
+import { getRelativeRequestType, getRelativeResponseType } from './utilities';
+
+/** Returns an iterable of generated BXPB clients from the given *.proto file and its descriptor. */
+export function* generateClientFiles(file: string, fileDescriptor: FileDescriptorProto):
+        Iterable<CodeGeneratorResponse.File> {
+    // If there are no services, don't generate anything.
+    if (fileDescriptor.getServiceList().length === 0) return;
+
+    const filePath = path.parse(file);
+    const generatedBaseName = `${filePath.name}_bxclients`;
+
+    const jsFile = new CodeGeneratorResponse.File();
+    jsFile.setName(path.format({
+        ...filePath,
+        base: undefined, // Must not specify base for name and ext to be used.
+        name: generatedBaseName,
+        ext: '.js',
+    }));
+    jsFile.setContent(generateClientJs(filePath, fileDescriptor));
+    yield jsFile;
+
+    const dtsFile = new CodeGeneratorResponse.File();
+    dtsFile.setName(path.format({
+        ...filePath,
+        base: undefined, // Must not specify base for name and ext to be used.
+        name: generatedBaseName,
+        ext: '.d.ts',
+    }));
+    dtsFile.setContent(generateClientDts(filePath, fileDescriptor));
+    yield dtsFile;
+}
+
+/** Returns the generated JavaScript source as a string. */
+function generateClientJs(filePath: path.ParsedPath, fileDescriptor: FileDescriptorProto): string {
+    return `
+import { ProtoClient, rpc } from 'bxpb-runtime/dist/client.js';
+import * as descriptors from './${filePath.name}_bxdescriptors.js';
+
+${fileDescriptor.getServiceList().map((service) => {
+    const serviceName = service.getName();
+    if (!serviceName) throw new Error(`${path.format(filePath)}: Service has no name.`);
+
+    return `
+/** Client for calling {@link descriptors.${serviceName}Service}. */
+export class ${serviceName}Client extends ProtoClient {
+    ${service.getMethodList().map((method) => {
+        const methodName = method.getName();
+        if (!methodName) throw new Error(`Method in service \`${serviceName}\` has no name!`);
+
+        return `
+    /** Invokes the {@link descriptors.${serviceName}Service.methods.${methodName}} method. */
+    async ${methodName}(req) {
+        return await rpc(this.sendMessage, descriptors.${serviceName}Service, descriptors.${serviceName}Service.methods.${methodName}, req);
+    }
+        `.trim();
+    }).join('\n\n    ' /* one indent */)}
+}
+    `.trim();
+}).join('\n\n')}
+    `.trim();
+}
+
+/** Returns the generated TypeScript definitions as a string. */
+function generateClientDts(filePath: path.ParsedPath, fileDescriptor: FileDescriptorProto): string {
+    return `
+import { ProtoClient } from 'bxpb-runtime/dist/client';
+import protos from './${filePath.name}_pb';
+import * as descriptors from './${filePath.name}_bxdescriptors';
+
+${fileDescriptor.getServiceList().map((service) => {
+    const serviceName = service.getName();
+    if (!serviceName) throw new Error(`${path.format(filePath)}: Service has no name.`);
+
+    return `
+/** Client for calling {@link descriptors.${serviceName}Service}. */
+export class ${serviceName}Client extends ProtoClient {
+    ${service.getMethodList().map((method) => {
+        const methodName = method.getName();
+        if (!methodName) throw new Error(`Method in service \`${serviceName}\` has no name!`);
+
+        return `
+    /** Invokes the {@link descriptors.${serviceName}Service.methods.${methodName}} method. */
+    ${methodName}(req: protos.${getRelativeRequestType(method)}): Promise<protos.${getRelativeResponseType(method)}>;
+        `.trim();
+    }).join('\n\n    ' /* one indent */)}
+}
+    `.trim();
+}).join('\n\n')}
+    `.trim();
+}
---
+++ packages/bxpb-protoc-plugin/src/generators/clients_spec.ts
@@ -0,0 +1,278 @@
+import { dummyFileDescriptor } from '../testing/dummies';
+import { generateClientFiles } from './clients';
+
+describe('clients', ()=> {
+    describe('generateClientFiles()', () => {
+        it('generates proto client files', () => {
+            const fileDescriptor = dummyFileDescriptor({
+                services: [
+                    {
+                        name: 'Foo',
+                        methods: [
+                            {
+                                name: 'Bar',
+                                inputType: 'BarRequest',
+                                outputType: 'BarResponse',
+                            },
+                        ],
+                    },
+                ],
+            });
+
+            const [ jsFile, dtsFile ] = Array.from(
+                generateClientFiles('foo.proto', fileDescriptor));
+
+            expect(jsFile.getName()).toBe('foo_bxclients.js');
+            expect(jsFile.getContent()).toBe(`
+import { ProtoClient, rpc } from 'bxpb-runtime/dist/client.js';
+import * as descriptors from './foo_bxdescriptors.js';
+
+/** Client for calling {@link descriptors.FooService}. */
+export class FooClient extends ProtoClient {
+    /** Invokes the {@link descriptors.FooService.methods.Bar} method. */
+    async Bar(req) {
+        return await rpc(this.sendMessage, descriptors.FooService, descriptors.FooService.methods.Bar, req);
+    }
+}
+            `.trim());
+
+            expect(dtsFile.getName()).toBe('foo_bxclients.d.ts');
+            expect(dtsFile.getContent()).toBe(`
+import { ProtoClient } from 'bxpb-runtime/dist/client';
+import protos from './foo_pb';
+import * as descriptors from './foo_bxdescriptors';
+
+/** Client for calling {@link descriptors.FooService}. */
+export class FooClient extends ProtoClient {
+    /** Invokes the {@link descriptors.FooService.methods.Bar} method. */
+    Bar(req: protos.BarRequest): Promise<protos.BarResponse>;
+}
+            `.trim());
+        });
+
+        it('generates multiple clients in a single file', () => {
+            const fileDescriptor = dummyFileDescriptor({
+                services: [
+                    {
+                        name: 'Foo',
+                        methods: [
+                            {
+                                name: 'FooMethod',
+                                inputType: 'FooMethodRequest',
+                                outputType: 'FooMethodResponse',
+                            },
+                        ],
+                    },
+                    {
+                        name: 'Bar',
+                        methods: [
+                            {
+                                name: 'BarMethod',
+                                inputType: 'BarMethodRequest',
+                                outputType: 'BarMethodResponse',
+                            },
+                        ],
+                    },
+                ],
+            });
+
+            const [ jsFile, dtsFile ] = Array.from(
+                generateClientFiles('foo.proto', fileDescriptor));
+            
+            expect(jsFile.getContent()).toBe(`
+import { ProtoClient, rpc } from 'bxpb-runtime/dist/client.js';
+import * as descriptors from './foo_bxdescriptors.js';
+
+/** Client for calling {@link descriptors.FooService}. */
+export class FooClient extends ProtoClient {
+    /** Invokes the {@link descriptors.FooService.methods.FooMethod} method. */
+    async FooMethod(req) {
+        return await rpc(this.sendMessage, descriptors.FooService, descriptors.FooService.methods.FooMethod, req);
+    }
+}
+
+/** Client for calling {@link descriptors.BarService}. */
+export class BarClient extends ProtoClient {
+    /** Invokes the {@link descriptors.BarService.methods.BarMethod} method. */
+    async BarMethod(req) {
+        return await rpc(this.sendMessage, descriptors.BarService, descriptors.BarService.methods.BarMethod, req);
+    }
+}
+            `.trim());
+
+            expect(dtsFile.getContent()).toBe(`
+import { ProtoClient } from 'bxpb-runtime/dist/client';
+import protos from './foo_pb';
+import * as descriptors from './foo_bxdescriptors';
+
+/** Client for calling {@link descriptors.FooService}. */
+export class FooClient extends ProtoClient {
+    /** Invokes the {@link descriptors.FooService.methods.FooMethod} method. */
+    FooMethod(req: protos.FooMethodRequest): Promise<protos.FooMethodResponse>;
+}
+
+/** Client for calling {@link descriptors.BarService}. */
+export class BarClient extends ProtoClient {
+    /** Invokes the {@link descriptors.BarService.methods.BarMethod} method. */
+    BarMethod(req: protos.BarMethodRequest): Promise<protos.BarMethodResponse>;
+}
+            `.trim());
+        });
+
+        it('generates multiple methods in a single client', () => {
+            const fileDescriptor = dummyFileDescriptor({
+                services: [
+                    {
+                        name: 'Foo',
+                        methods: [
+                            {
+                                name: 'Bar',
+                                inputType: 'BarRequest',
+                                outputType: 'BarResponse',
+                            },
+                            {
+                                name: 'Baz',
+                                inputType: 'BazRequest',
+                                outputType: 'BazResponse',
+                            },
+                        ],
+                    },
+                ],
+            });
+
+            const [ jsFile, dtsFile ] = Array.from(
+                generateClientFiles('foo.proto', fileDescriptor));
+            
+            expect(jsFile.getContent()).toBe(`
+import { ProtoClient, rpc } from 'bxpb-runtime/dist/client.js';
+import * as descriptors from './foo_bxdescriptors.js';
+
+/** Client for calling {@link descriptors.FooService}. */
+export class FooClient extends ProtoClient {
+    /** Invokes the {@link descriptors.FooService.methods.Bar} method. */
+    async Bar(req) {
+        return await rpc(this.sendMessage, descriptors.FooService, descriptors.FooService.methods.Bar, req);
+    }
+
+    /** Invokes the {@link descriptors.FooService.methods.Baz} method. */
+    async Baz(req) {
+        return await rpc(this.sendMessage, descriptors.FooService, descriptors.FooService.methods.Baz, req);
+    }
+}
+            `.trim());
+
+            expect(dtsFile.getContent()).toBe(`
+import { ProtoClient } from 'bxpb-runtime/dist/client';
+import protos from './foo_pb';
+import * as descriptors from './foo_bxdescriptors';
+
+/** Client for calling {@link descriptors.FooService}. */
+export class FooClient extends ProtoClient {
+    /** Invokes the {@link descriptors.FooService.methods.Bar} method. */
+    Bar(req: protos.BarRequest): Promise<protos.BarResponse>;
+
+    /** Invokes the {@link descriptors.FooService.methods.Baz} method. */
+    Baz(req: protos.BazRequest): Promise<protos.BazResponse>;
+}
+            `.trim());
+        });
+
+        it('generates nothing if there are no services in the file', () => {
+            const fileDescriptor = dummyFileDescriptor({
+                services: [], // No services in the file.
+            });
+
+            const generatedFiles = Array.from(generateClientFiles('foo.proto', fileDescriptor));
+
+            expect(generatedFiles).toEqual([]);
+        });
+
+        it('generates files in subdirectory', () => {
+            const [ jsFile, dtsFile ] = Array.from(generateClientFiles(
+                'hello/world/foo.proto', dummyFileDescriptor()));
+            
+            // Output files should be in the same subdirectory.
+            expect(jsFile.getName()).toBe('hello/world/foo_bxclients.js');
+            expect(dtsFile.getName()).toBe('hello/world/foo_bxclients.d.ts');
+        });
+
+        it('throws when given a method with no name', () => {
+            const fileDescriptor = dummyFileDescriptor({
+                services: [
+                    {
+                        name: 'Foo',
+                        methods: [
+                            {
+                                name: null, // Caller forgot to set a name.
+                            },
+                        ],
+                    },
+                ],
+            });
+
+            expect(() => Array.from(generateClientFiles('foo.proto', fileDescriptor)))
+                .toThrowMatching(
+                    (err: Error) => err.message.includes('Method in service `Foo` has no name!'),
+                )
+            ;
+        });
+
+        it('throws when given a method with no input type', () => {
+            const fileDescriptor = dummyFileDescriptor({
+                services: [
+                    {
+                        methods: [
+                            {
+                                name: 'Bar',
+                                inputType: null, // Caller forgot to set an input type.
+                            },
+                        ],
+                    },
+                ],
+            });
+
+            expect(() => Array.from(generateClientFiles('foo.proto', fileDescriptor)))
+                .toThrowMatching(
+                    (err: Error) => err.message.includes('Method `Bar` is missing an input type!'),
+                )
+            ;
+        });
+
+        it('throws when given a method with no output type', () => {
+            const fileDescriptor = dummyFileDescriptor({
+                services: [
+                    {
+                        methods: [
+                            {
+                                name: 'Bar',
+                                outputType: null,
+                            },
+                        ],
+                    },
+                ],
+            });
+
+            expect(() => Array.from(generateClientFiles('foo.proto', fileDescriptor)))
+                .toThrowMatching(
+                    (err: Error) => err.message.includes('Method `Bar` is missing an output type!'),
+                )
+            ;
+        });
+
+        it('throws when given a service with no name', () => {
+            const fileDescriptor = dummyFileDescriptor({
+                services: [
+                    {
+                        name: null, // Caller forgot to set a name.
+                    },
+                ],
+            });
+
+            expect(() => Array.from(generateClientFiles('foo.proto', fileDescriptor)))
+                .toThrowMatching(
+                    (err: Error) => err.message.includes('foo.proto: Service has no name.'),
+                )
+            ;
+        });
+    });
+});
