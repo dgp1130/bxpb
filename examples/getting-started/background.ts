2c1a8fdcc0b7271b10662e186268db2a7bf774d7
Doug Parker <dgp1130@users.noreply.github.com>
2020-06-15 21:42:45 -0700

    Part 3 checkpoint of getting started documentation.
    After completing part 3, you should be at this point.

--- examples/getting-started/background.ts
+++ examples/getting-started/background.ts
@@ -1,3 +1,18 @@
 // Runs in a background context.
 
+import { serveGreeter } from './proto/greeter_bxservices';
+import { GreetRequest, GreetResponse } from './proto/greeter_pb';
+
 console.log('I am the background script!');
+
+// Run `Greeter`, listening for requests using the `onMessage` event.
+serveGreeter(chrome.runtime.onMessage, {
+    // Handle a client request for the `Greet()` method.
+    async Greet(req: GreetRequest): Promise<GreetResponse> {
+        console.log(`Received request for name: "${req.getName()}"`);
+
+        const res = new GreetResponse();
+        res.setMessage(`Hello, ${req.getName()}!`);
+        return res;
+    },
+});
--- examples/getting-started/popup.ts
+++ examples/getting-started/popup.ts
@@ -1,3 +1,20 @@
 // Runs in a popup context.
 
+import { GreeterClient } from './proto/greeter_bxclients';
+import { GreetRequest } from './proto/greeter_pb';
+
 console.log('I am the popup!');
+
+// An immediately-invoked function expression (IIFE) to get an asynchronous context to use `await`.
+(async () => {
+    // Create a client which sends a message that will trigger `chrome.runtime.onMessage`.
+    const client = new GreeterClient(chrome.runtime.sendMessage);
+
+    const req = new GreetRequest();
+    req.setName('Dave');
+    
+    // Send the RPC and get the response.
+    const res = await client.Greet(req);
+
+    console.log(`Message from background script: "${res.getMessage()}"`);
+})();
